34231e3b7ced5a507043210ae8f15341c4df83bc
GEO-HACK <geoffreykirumba0@gmail.com>
2024-06-12 22:46:24 +0300

    second commit

---
+++ Weather app/javascript.js
@@ -0,0 +1,53 @@
+const apiKey = "3a4c32579ad37ae118d9cd889ea7ae40";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+
+async function checkWeather(city){
+    const response = await fetch(`${apiUrl} + ${city} + &appid=${apiKey}`);
+
+    if(response.status== 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+
+    var data = await response.json();
+
+    
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".temp").innerText = Math.round(data.main.temp )+ "°C";
+    // document.querySelector(".weather").innerText = data.weather[0].main;
+    // document.querySelector(".min").innerText = data.main.temp_min;
+    // document.querySelector(".max").innerText = data.main.temp_max;
+    document.querySelector(".humidity").innerText = data.main.humidity + "%";
+    document.querySelector(".wind").innerText = data.wind.speed + "km/h";
+
+    if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    
+    }else if(data.weather[0].main == "Clouds"){
+       weatherIcon.src = "images/clouds.png";
+    
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    
+    }else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);//will give the city name in the input field
+    searchBox.value = "";
+}
+);
